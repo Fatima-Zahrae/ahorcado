6e361a81137625c13444f6cca30ed6efff8bef09
Fatima-Zahrae <fatimaelkessab17@gmail.com>
2018-01-10 11:30:52 +0100

    falta almacenar las letras

--- juego.php
+++ juego.php
@@ -1,28 +1,28 @@
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 $incognita = htmlspecialchars($_POST['incognita']);
+$letra = isset($_POST['letra']) ? htmlspecialchars($_POST['letra']): '';
 
 ?>
 
 <h1>
+    
 <?php
-//echo $incognita;
-
-    for( $i=1 ; $i<= strlen($incognita); $i++){
-        echo " _ ";
+    for( $i=0 ; $i<= strlen($incognita)-1; $i++){
+        if($incognita[$i] === $letra){
+            echo $letra=$letra."";
+        }else{
+            echo " _ ";
+        }
     };
 ?>
-<h1>
+     
+</h1>
 
     <form action="" method="post">
         <input type="hidden" name="incognita" value="<?php echo $incognita?>"/>
+        <!--<input type="text" name="letra" value="<?php echo $letra?>"/> -->
         <label for="incognita"> Escribe una letra</label>
-        <input type="text" maxlenght="1" size="1" name="letra" />
+        <input type="text" maxlenght="1" size="1" name="letra" pattern="[A-Za-z]{1}"/>
         <input type="submit" value="Enviar" />
     </form>
 
